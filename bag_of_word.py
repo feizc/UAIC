ca963dbbd97c2b25cd69f3d7619816724a6c7573
zcfei <3300949577@qq.com>
2020-12-13 18:37:16 +0800

    model

--- bag_of_word.py
+++ bag_of_word.py
@@ -1,4 +1,27 @@
-from transformers import BertTokenizer 
+from transformers import BertTokenizer, BertConfig, BertModel, BertPreTrainedModel 
+import torch.nn as nn
+import torch
+
+
+# Uncertainty-aware image-conditioned bag-of-words
+class BagofWords(BertPreTrainedModel):
+    def __init__(self, config):
+        super(BagofWords, self).__init__(config)
+        self.transformer = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.vocab_size)
+        self.init_weights()
+    
+    def forward(self, img_embs, labels):
+        transformer_outputs = self.transformer(inputs_embeds=img_embs)
+        hidden_states = transformer_outputs[1]
+        pool_outputs = self.dropout(hidden_states)
+        pool_outputs = self.classifier(pool_outputs)
+
+        criterion = nn.MultiLabelSoftMarginLoss()
+        loss = criterion(pool_outputs, labels)
+
+        return loss
 
 
 
