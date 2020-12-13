f79dd23ff2959632cb6f9462564f1aa7886dad77
zcfei <3300949577@qq.com>
2020-12-13 16:18:22 +0800

    haha

--- bag_of_word.py
+++ bag_of_word.py
@@ -1,2 +1,11 @@
+from transformers import BertTokenizer 
+
+
+
+if __name__ == "__main__":
+    tokenizer = BertTokenizer('data/vocab.txt')
+    s = 'a cat shsiss dog'
+    s = tokenizer.tokenize(s)
+    print(tokenizer.convert_tokens_to_ids(s))
 
 
--- utilis.py
+++ utilis.py
@@ -5,7 +5,7 @@ from collections import Counter
 import nltk
 
 # Create vocabulary from dataset
-def create_vocabulary(path, min_word_freq=3):
+def create_vocabulary(path, min_word_freq=5):
     train_path = os.path.join(path, 'captions_train2014.json')
 
     with open(train_path, 'r') as j:
@@ -15,20 +15,20 @@ def create_vocabulary(path, min_word_freq=3):
 
     word_freq = Counter()
     for ann in data['annotations']:
-        caption = nltk.word_tokenize(ann['caption'].lower())
-        pos_tags = nltk.pos_tag(caption)
+        #caption = nltk.word_tokenize(ann['caption'].lower())
+        #pos_tags = nltk.pos_tag(caption)
+        caption = ann['caption'].lower().split()
         word_freq.update(caption)
 
     words = [w for w in word_freq.keys() if word_freq[w] > min_word_freq]
 
-    refine_words = []
-    for word, pos in pos_tags:
-        if (pos == 'NN' or pos == 'VB' or pos == 'JJ' ) and word in words:
-            
-            refine_words.append(word)
+    #refine_words = []
+    #for word, pos in pos_tags:
+    #    if (pos == 'NN' or pos == 'VB' or pos == 'JJ' ) and word in words:
+    #        refine_words.append(word)
 
     with open(os.path.join(path, 'vocab.txt'), 'w', encoding='utf-8') as f:
-        for word in refine_words:
+        for word in words:
             if '\'' in word:
                 f.write(word.split('\'')[0] + '\n')
             elif word[-1] == '.' or word[-1] == ',':
