b5eab2ad31d6738674da92ed7150522b643b4f25
feizc <feizhengcong@ict.ac.cn>
2020-12-14 21:17:26 +0800

    generator

--- train.py
+++ train.py
@@ -1,2 +1,24 @@
 import json 
 import torch 
+import os 
+
+from transformers import BertTokenizer, BertForMaskedLM 
+
+def train():
+    model_path = 'model'
+
+    tokenizer = BertTokenizer.from_pretrained(model_path)
+    model = BertForMaskedLM.from_pretrained(model_path)
+
+    
+
+    '''
+    torch.save(model.state_dict(), os.path.join(model_path, 'pytorch_model.bin'))
+    model.config.to_json_file(os.path.join(model_path, 'config.json'))
+    tokenizer.save_vocabulary(model_path)
+    '''
+
+
+if __name__ == "__main__":
+    train()
+
